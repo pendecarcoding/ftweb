958ec89e3ad5ddf39baccc209b16f82af4a1b1ea
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2023-08-16 12:56:17 +0800

    update the icon

--- resources/views/gosford/frontend/option_sumary.blade.php
+++ resources/views/gosford/frontend/option_sumary.blade.php
@@ -153,11 +153,11 @@
 
                                                 </div>
                                                 <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-                                                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                  <i style="color: #BF1D2C;font-size: 50px;" class="fa fa-angle-left"></i>
                                                   <span class="visually-hidden">Previous</span>
                                                 </button>
                                                 <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-                                                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                                <i style="color: #BF1D2C;font-size: 50px;" class="fa fa-angle-right"></i>
                                                   <span class="visually-hidden">Next</span>
                                                 </button>
                                               </div>
