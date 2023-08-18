4a78d3db74dc65bedd1da6e856c5ef81f8720385
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2023-08-18 10:35:34 +0800

    Design Research Page done

--- resources/views/gosford/frontend/research.blade.php
+++ resources/views/gosford/frontend/research.blade.php
@@ -52,11 +52,122 @@
         </section>
 
         <section class="pt-5 pb-5">
-            <div class="container">
-                <div class="row">
+            <div style="display: flex;flex-direction: column;">
+                <div style="position: relative;">
+                    <div class="row" style="padding-left: 0px;padding-right: 0px;--bs-gutter-x: 0;">
+                        <div class="col-md-6 col-lg-6 col-sm-12">
+                            <div style="padding: 15%;">
+                            <h5 style="color: #E20000;">CUT DESIGN | MANUFACTURING.</h5>
+                            <p style="text-align: justify;">We are delighted to present our latest masterpiece to you – high-quality automotive leather. This copy will showcase the true charm of this exquisite creation.
+                                Designed exclusively for car owners, our top-tier leather embodies our unwavering commitment to perfection. Each piece of leather material undergoes rigorous selection, ensuring that only the finest, handpicked leather finds its way into our car interiors. Your distinguished status deserves nothing less than the most elegant indulgence.
+                                The high-quality leather boasts an astonishingly soft touch, providing a luxurious sensation that soothes both body and mind. Selected from the finest raw materials, the leather's durability surpasses ordinary standards, maintaining its shine and color as if new for a prolonged period. Whether your journey is long or short, this elegance and refinement will accompany you all the way.</p>
+                            </div>
 
+
+                        </div>
+                        <div class="col-md-6 col-lg-6 col-sm-12" style="position: relative;overflow: hidden;">
+                            <img style="width: 100%;" src="/public/go_system/images/research/cut_img.svg" alt="">
+                        </div>
+                    </div>
                 </div>
+                <div style="position: relative;">
+                    <div class="row" style="padding-left: 0px;padding-right: 0px;--bs-gutter-x: 0;">
+                        <div class="col-md-6 col-lg-6 col-sm-12" style="position: relative;overflow: hidden;">
+                            <img style="width: 100%;" src="/public/go_system/images/research/cut_img_2.svg" alt="">
+                        </div>
+                        <div class="col-md-6 col-lg-6 col-sm-12">
+                            <div style="padding: 15%;">
+                            <h5 style="color: #E20000;">CUT DESIGN | MANUFACTURING.</h5>
+                            <p style="text-align: justify;">We are delighted to present our latest masterpiece to you – high-quality automotive leather. This copy will showcase the true charm of this exquisite creation.
+                                Designed exclusively for car owners, our top-tier leather embodies our unwavering commitment to perfection. Each piece of leather material undergoes rigorous selection, ensuring that only the finest, handpicked leather finds its way into our car interiors. Your distinguished status deserves nothing less than the most elegant indulgence.
+                                The high-quality leather boasts an astonishingly soft touch, providing a luxurious sensation that soothes both body and mind. Selected from the finest raw materials, the leather's durability surpasses ordinary standards, maintaining its shine and color as if new for a prolonged period. Whether your journey is long or short, this elegance and refinement will accompany you all the way.</p>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div style="position: relative;">
+                    <div class="row" style="padding-left: 0px;padding-right: 0px;--bs-gutter-x: 0;">
+                        <div class="col-md-6 col-lg-6 col-sm-12">
+                            <div style="padding: 15%;">
+                            <h5 style="color: #E20000;">CUT DESIGN | MANUFACTURING.</h5>
+                            <p style="text-align: justify;">We are delighted to present our latest masterpiece to you – high-quality automotive leather. This copy will showcase the true charm of this exquisite creation.
+                                Designed exclusively for car owners, our top-tier leather embodies our unwavering commitment to perfection. Each piece of leather material undergoes rigorous selection, ensuring that only the finest, handpicked leather finds its way into our car interiors. Your distinguished status deserves nothing less than the most elegant indulgence.
+                                The high-quality leather boasts an astonishingly soft touch, providing a luxurious sensation that soothes both body and mind. Selected from the finest raw materials, the leather's durability surpasses ordinary standards, maintaining its shine and color as if new for a prolonged period. Whether your journey is long or short, this elegance and refinement will accompany you all the way.</p>
+                            </div>
+
+
+                        </div>
+                        <div class="col-md-6 col-lg-6 col-sm-12" style="position: relative;overflow: hidden;">
+                            <iframe width="930" height="523" src="https://www.youtube.com/embed/qVcqi1GuEiw" title="HOW TO CUT &amp; ENGRAVE LEATHER | WATTSAN CNC MACHINE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <div style="position: relative;">
+                    <div class="row" style="padding-left: 0px;padding-right: 0px;--bs-gutter-x: 0;">
+                        <div class="col-md-6 col-lg-6 col-sm-12" style="position: relative;overflow: hidden;">
+                            <img style="width: 100%;" src="/public/go_system/images/research/cut_img_2.svg" alt="">
+                        </div>
+                        <div class="col-md-6 col-lg-6 col-sm-12">
+                            <div style="padding: 15%;">
+                            <h5 style="color: #E20000;">CUT DESIGN | MANUFACTURING.</h5>
+                            <p style="text-align: justify;">We are delighted to present our latest masterpiece to you – high-quality automotive leather. This copy will showcase the true charm of this exquisite creation.
+                                Designed exclusively for car owners, our top-tier leather embodies our unwavering commitment to perfection. Each piece of leather material undergoes rigorous selection, ensuring that only the finest, handpicked leather finds its way into our car interiors. Your distinguished status deserves nothing less than the most elegant indulgence.
+                                The high-quality leather boasts an astonishingly soft touch, providing a luxurious sensation that soothes both body and mind. Selected from the finest raw materials, the leather's durability surpasses ordinary standards, maintaining its shine and color as if new for a prolonged period. Whether your journey is long or short, this elegance and refinement will accompany you all the way.</p>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div style="position: relative;">
+                    <div class="row" style="padding-left: 0px;padding-right: 0px;--bs-gutter-x: 0;">
+                        <div class="col-md-6 col-lg-6 col-sm-12">
+                            <div style="padding: 15%;">
+                            <h5 style="color: #E20000;">CUT DESIGN | MANUFACTURING.</h5>
+                            <p style="text-align: justify;">We are delighted to present our latest masterpiece to you – high-quality automotive leather. This copy will showcase the true charm of this exquisite creation.
+                                Designed exclusively for car owners, our top-tier leather embodies our unwavering commitment to perfection. Each piece of leather material undergoes rigorous selection, ensuring that only the finest, handpicked leather finds its way into our car interiors. Your distinguished status deserves nothing less than the most elegant indulgence.
+                                The high-quality leather boasts an astonishingly soft touch, providing a luxurious sensation that soothes both body and mind. Selected from the finest raw materials, the leather's durability surpasses ordinary standards, maintaining its shine and color as if new for a prolonged period. Whether your journey is long or short, this elegance and refinement will accompany you all the way.</p>
+                            </div>
+
+
+                        </div>
+                        <div class="col-md-6 col-lg-6 col-sm-12" style="position: relative;overflow: hidden;">
+                            <img style="width: 100%;" src="/public/go_system/images/research/cut_img.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div style="position: relative;">
+                    <div class="row" style="padding-left: 0px;padding-right: 0px;--bs-gutter-x: 0;">
+                        <div class="col-md-6 col-lg-6 col-sm-12" style="position: relative;overflow: hidden;">
+                            <img style="width: 100%;" src="/public/go_system/images/research/cut_img_3.svg" alt="">
+                        </div>
+                        <div class="col-md-6 col-lg-6 col-sm-12">
+                            <div style="padding: 15%;">
+                            <h5 style="color: #E20000;">CUT DESIGN | MANUFACTURING.</h5>
+                            <p style="text-align: justify;">We are delighted to present our latest masterpiece to you – high-quality automotive leather. This copy will showcase the true charm of this exquisite creation.
+                                Designed exclusively for car owners, our top-tier leather embodies our unwavering commitment to perfection. Each piece of leather material undergoes rigorous selection, ensuring that only the finest, handpicked leather finds its way into our car interiors. Your distinguished status deserves nothing less than the most elegant indulgence.
+                                The high-quality leather boasts an astonishingly soft touch, providing a luxurious sensation that soothes both body and mind. Selected from the finest raw materials, the leather's durability surpasses ordinary standards, maintaining its shine and color as if new for a prolonged period. Whether your journey is long or short, this elegance and refinement will accompany you all the way.</p>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+
+
             </div>
+
         </section>
 
 
