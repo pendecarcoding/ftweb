8f7f29f14aa0b20cdb9fad54fab4799d253f6639
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2023-08-18 10:01:42 +0800

    add page research image

--- app/Http/Controllers/ace/AceController.php
+++ app/Http/Controllers/ace/AceController.php
@@ -124,6 +124,10 @@ class AceController extends Controller
 
     public function page($page){
         switch ($page) {
+            case 'Research & Development':
+            $slider      =  Slider::select('sliders.id as id','sliders.caption as caption','sliders.sub_caption','uploads.file_name as file_name')->join('uploads','uploads.id','sliders.image')->where('sliders.type','RESEARCH')->get();
+            return view('gosford.frontend.research',compact('slider'));
+            break;
             case 'mypage';
             $slider      =  Slider::select('sliders.id as id','sliders.caption as caption','sliders.sub_caption','uploads.file_name as file_name')->join('uploads','uploads.id','sliders.image')->where('sliders.type','PERSONAL')->get();
             return view('gosford.frontend.choice_design',compact('slider'));
