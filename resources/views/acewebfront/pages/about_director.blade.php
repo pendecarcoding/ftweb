38f9ceb0d2df19dc70aef0dbf3595ea9ffaf864b
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2023-08-19 23:50:46 +0800

    update the image modal size

--- resources/views/acewebfront/pages/about_director.blade.php
+++ resources/views/acewebfront/pages/about_director.blade.php
@@ -1,108 +1,114 @@
 @extends('acewebfront.layouts')
 @section('content')
-<main>
-  <section class="bg-banner">
-    <img
-                    title="about-banner"
-                    class="img-responsive"
-                    src="{{ static_asset('aceweb') }}/assets/img/about_director.png"
-                    alt=""
-                />
-  </section>
-      <section class="gtp-anouncements">
-        <div class="content-ace">
-          <div class="wrap-content">
-            <div style="padding-top: 0px" class="ace-isi about">
-              <div class="row">
-                <div class="col-md-12">
-                    <div class="title-ace">
-                        ABOUT > BOARD OF DIRECTORS
-                      <span class="h-dash" style="font-weight: bold">—</span>
-                    </div>
-                    <div  class="col-md-12 col-sm-12 aos-init aos-animate">
-                        <h1>Meet our board of directors
-                        </h1>
-                      </div>
-                  </div>
-              </div>
-              <!-- @foreach($data as $i => $v)
-              <div data-aos="fade-up" data-aos-delay="{{$i+1}}00" class="row directors_list">
-                <div class="col-md-6">
-                    <img class="img-responsive" src="{{getimage($v->foto)}}" alt="">
-                </div>
-                <div class="col-md-6">
-                    <div>
-                        <h2 class="position-name">{{$v->name}}</h2>
-                        <div class="position-about">{{$v->position}}</div>
-                        {!!$v->content!!}
-                    </div>
-                </div>
-              </div>
-              @endforeach -->
-
-              <div class="container mt-5 mb-5">
-     <div class="row g-2">
-     @foreach($data as $i => $v)
-    <div data-bs-toggle="modal" data-bs-target="#readboard{{$v->id}}" data-aos="fade-up" data-aos-delay="{{$i+1}}00" class="col-md-4 aos-init aos-animate">
-        <div style="height: 400px;position: relative;
-        overflow: hidden;" class="card text-center board-director">
-            <div style="    position: relative;
-            height: 280px;
-            border-radius: 9px;
-            overflow: hidden;"> <img src="{{getimage($v->foto)}}" width="100%" > </div>
-            <br>
-            <h5 class="mb-0">{{$v->name}}</h5> <small>{{$v->position}}</small>
-        </div>
-    </div>
-    <div class="modal fade" id="readboard{{$v->id}}">
-        <div class="modal-dialog modal-dialog-centered modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                </div>
-                <div class="modal-body">
-
-                    <div id="k2Container" class="itemView team-popup">
+    <main>
+        <section class="bg-banner">
+            <img title="about-banner" class="img-responsive" src="{{ static_asset('aceweb') }}/assets/img/about_director.png"
+                alt="" />
+        </section>
+        <section class="gtp-anouncements">
+            <div class="content-ace">
+                <div class="wrap-content">
+                    <div style="padding-top: 0px" class="ace-isi about">
                         <div class="row">
-                            <div class="col-xs-12 col-sm-4">
-                                <!-- Item Image -->
-                                <div class="itemImageBlock">
-                                    <span class="itemImage">
-                                            <img style="height: 300px;" src="{{getimage($v->foto)}}" class="img-responsive">
-                                    </span>
+                            <div class="col-md-12">
+                                <div class="title-ace">
+                                    ABOUT > BOARD OF DIRECTORS
+                                    <span class="h-dash" style="font-weight: bold">—</span>
                                 </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-8">
-                                <!-- Item text -->
-                                <div class="itemFullText" style="height:300px;overflow-y:scroll;position:relative;padding: 0px 10px;">
-                                    <h2>{{$v->name}}</h2>
-                                    <p>{{$v->position}}</p>
-                                    {!! $v->content !!}
+                                <div class="col-md-12 col-sm-12 aos-init aos-animate">
+                                    <h1>Meet our board of directors
+                                    </h1>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    </div>
+                        <!-- @foreach ($data as $i => $v)
+    <div data-aos="fade-up" data-aos-delay="{{ $i + 1 }}00" class="row directors_list">
+                        <div class="col-md-6">
+                            <img class="img-responsive" src="{{ getimage($v->foto) }}" alt="">
+                        </div>
+                        <div class="col-md-6">
+                            <div>
+                                <h2 class="position-name">{{ $v->name }}</h2>
+                                <div class="position-about">{{ $v->position }}</div>
+                                {!! $v->content !!}
+                            </div>
+                        </div>
+                      </div>
+    @endforeach -->
 
-                <div class="modal-footer" style="background-color: brown;">
+                        <div class="container mt-5 mb-5">
+                            <div class="row g-2">
+                                @foreach ($data as $i => $v)
+                                    <div data-bs-toggle="modal" data-bs-target="#readboard{{ $v->id }}"
+                                        data-aos="fade-up" data-aos-delay="{{ $i + 1 }}00"
+                                        class="col-md-4 aos-init aos-animate">
+                                        <div style="height: 400px;position: relative;
+        overflow: hidden;"
+                                            class="card text-center board-director">
+                                            <div
+                                                style="    position: relative;
+            height: 280px;
+            border-radius: 9px;
+            overflow: hidden;">
+                                                <img src="{{ getimage($v->foto) }}" width="100%">
+                                            </div>
+                                            <br>
+                                            <h5 class="mb-0">{{ $v->name }}</h5> <small>{{ $v->position }}</small>
+                                        </div>
+                                    </div>
+                                    <div class="modal fade" id="readboard{{ $v->id }}">
+                                        <div class="modal-dialog modal-dialog-centered modal-lg">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="btn-close"
+                                                        data-bs-dismiss="modal"></button>
+                                                </div>
+                                                <div class="modal-body">
 
+                                                    <div id="k2Container" class="itemView team-popup">
+                                                        <div class="row">
+                                                            <div class="col-xs-12 col-sm-4">
+                                                                <!-- Item Image -->
+                                                                <div class="itemImageBlock">
+                                                                    <span class="itemImage">
+                                                                        <img style="height: 300px;width: 100%;margin-bottom: 20px;"
+                                                                            src="{{ getimage($v->foto) }}"
+                                                                            class="img-responsive">
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-xs-12 col-sm-8">
+                                                                <!-- Item text -->
+                                                                <div class="itemFullText"
+                                                                    style="height:300px;overflow-y:scroll;position:relative;padding: 0px 10px;">
+                                                                    <h2>{{ $v->name }}</h2>
+                                                                    <p>{{ $v->position }}</p>
+                                                                    {!! $v->content !!}
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="modal-footer" style="background-color: brown;">
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+
+
+
+
+                            </div>
+                        </div>
+
+
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-@endforeach
-
-
-
-
-    </div>
-</div>
-
-
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
 
     </main>
 @endsection
