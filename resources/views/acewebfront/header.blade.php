dc97793e62497b0e2915b536ea63ccbfd3312ac7
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2023-08-21 15:00:38 +0800

    update

--- resources/views/acewebfront/header.blade.php
+++ resources/views/acewebfront/header.blade.php
@@ -3,10 +3,13 @@
         <i class="material-icons menu-icon"></i>
         <i class="material-icons times-icon" style="display: none;">✕</i>
     </div>
+    @if(Session::get('gystem_login'))
+    @else
     <div data-bs-toggle="modal" data-bs-target="#myLogin" aria-expanded="false" role="button" tabindex="0" class="mdl-layout__drawer-button-login" id="navbarToggle">
         <i style="font-size: 25px;" class="fa fa-user-circle" aria-hidden="true"></i>
 
     </div>
+    @endif
     <div aria-expanded="false" role="button" tabindex="0" class="">
         <a href="{{ route('home') }}"><img id="acetopbar" class="acetopbar"
             src="{{ uploaded_asset(get_setting('system_logo_white')) }}" /></a>
