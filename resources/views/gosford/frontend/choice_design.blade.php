022fc9549e6b8dbf13c0f520fa0ef4caeb818fdf
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2023-09-13 11:55:49 +0800

    update

--- resources/views/gosford/frontend/choice_design.blade.php
+++ resources/views/gosford/frontend/choice_design.blade.php
@@ -198,6 +198,27 @@
 
             </div>
         </section>
+        <section>
+            <div class="section-full about-box content-inner section-bg-gosford-color">
+                <div class="container">
+                    <div class="row">
+                        <div data-aos="fade-right" data-aos-delay="100" class="col-sm-12 col-lg-6 m-b30 p-r50">
+                            <div class="section-head text-left" style="    margin: 15% 0px;">
+                                <h2 class="text-capitalize" style="color: black;font-family: 'Space Grotesk';font-weight: bold;">MISSION</h2>
+                                <p class="font-patner" style="color:black">Combining people innovation with most cutting edge manufacturing solution and provide best design with highest quality product to customers.</p>
+                            </div>
+
+                        </div>
+                        <div class="col-sm-12 col-lg-6 m-b30">
+                            <div class="video-box" style="padding: 50px 0px;">
+                                <iframe width="100%" height="450px" src="https://www.youtube.com/embed/WEQDCxT6RFY" title="Gosford, Johor Bahru" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
     </main>
     <script>
