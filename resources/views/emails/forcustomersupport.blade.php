4b4319d74427a11de1d70d1e279ba021cfe057a3
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2024-01-17 07:41:06 +0700

    not use bold

---
+++ resources/views/emails/forcustomersupport.blade.php
@@ -0,0 +1,159 @@
+<table class="nl-container" border="0" cellpadding="0" cellspacing="0" role="presentation"
+    style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #fff;">
+    <tbody>
+        <tr>
+            <td>
+                <table class="row row-1" align="center" width="100%" border="0" cellpadding="0" cellspacing="0"
+                    role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <table class="row-content stack" align="center" border="0" cellpadding="0"
+                                    cellspacing="0" role="presentation"
+                                    style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 600px; margin: 0 auto;"
+                                    width="600">
+                                    <tbody>
+                                        <tr>
+                                            <td class="column column-1" width="100%"
+                                                style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+                                                <table class="heading_block block-1" width="100%" border="0"
+                                                    cellpadding="10" cellspacing="0" role="presentation"
+                                                    style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td class="pad">
+                                                                <p
+                                                                    style="margin: 0; direction: ltr; font-family: Arial, Helvetica, sans-serif; font-size: 17px; letter-spacing: normal; line-height: 120%; text-align: left; margin-top: 0; margin-bottom: 0;">
+                                                                    <span class="tinyMce-placeholder"><span
+                                                                            style="">{{ $array['dear'] }}</span></span>
+                                                                    </p>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="row row-2" align="center" width="100%" border="0" cellpadding="0" cellspacing="0"
+                    role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <table class="row-content stack" align="center" border="0" cellpadding="0"
+                                    cellspacing="0" role="presentation"
+                                    style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000; width: 600px; margin: 0 auto;"
+                                    width="600">
+                                    <tbody>
+                                        <tr>
+                                            <td class="column column-1" width="100%"
+                                                style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+                                                <table class="text_block block-1" width="100%" border="0"
+                                                    cellpadding="10" cellspacing="0" role="presentation"
+                                                    style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td class="pad">
+                                                                <div style="font-family: sans-serif">
+                                                                    <div class=""
+                                                                        style="font-size: 12px; font-family: Arial, Helvetica, sans-serif; mso-line-height-alt: 14.399999999999999px; color: #555555; line-height: 1.2;">
+                                                                        <p
+                                                                            style="margin: 0; font-size: 16px; text-align: left; mso-line-height-alt: 19.2px;">
+                                                                            <span style="color:#000000;">{{ $array['opening'] }}</span>
+                                                                        </p>
+                                                                    </div>
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                                <table class="text_block block-2" width="100%" border="0"
+                                                    cellpadding="10" cellspacing="0" role="presentation"
+                                                    style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td class="pad">
+                                                                <div style="font-family: sans-serif">
+                                                                    <div class=""
+                                                                        style="font-size: 12px; font-family: Arial, Helvetica, sans-serif; mso-line-height-alt: 14.399999999999999px; color: #555555; line-height: 1.2;">
+                                                                        <p
+                                                                            style="margin: 0; font-size: 14px; text-align: left; mso-line-height-alt: 16.8px;">
+                                                                            {!! $array['content'] !!}</p>
+                                                                    </div>
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                                <table class="button_block block-3" width="100%" border="0"
+                                                    cellpadding="10" cellspacing="0" role="presentation"
+                                                    style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td class="pad">
+                                                                <div class="alignment" align="left">
+                                                                    <!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" style="height:42px;width:80px;v-text-anchor:middle;" arcsize="10%" stroke="false" fillcolor="#3AAEE0"><w:anchorlock/><v:textbox inset="0px,0px,0px,0px"><center style="color:#ffffff; font-family:Arial, sans-serif; font-size:16px"><![endif]-->
+                                                                    <a href="mailto:{{$array['email']}}" target="_blank"><div
+                                                                        style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#3AAEE0;border-radius:4px;width:auto;border-top:0px solid transparent;font-weight:undefined;border-right:0px solid transparent;border-bottom:0px solid transparent;border-left:0px solid transparent;padding-top:5px;padding-bottom:5px;font-family:Arial, Helvetica, sans-serif;font-size:16px;text-align:center;mso-border-alt:none;word-break:keep-all;">
+                                                                        <span
+                                                                            style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;letter-spacing:normal;"><span
+                                                                                style="margin: 0; word-break: break-word; line-height: 32px;">Reply</span></span>
+
+                                                                    </div>
+                                                                    </a>
+                                                                    <!--[if mso]></center></v:textbox></v:roundrect><![endif]-->
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="row row-3" align="center" width="100%" border="0" cellpadding="0"
+                    cellspacing="0" role="presentation"
+                    style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff;">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <table class="row-content stack" align="center" border="0" cellpadding="0"
+                                    cellspacing="0" role="presentation"
+                                    style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000; width: 600px; margin: 0 auto;"
+                                    width="600">
+                                    <tbody>
+                                        <tr>
+                                            <td class="column column-1" width="100%"
+                                                style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+                                                <table class="icons_block block-1" width="100%" border="0"
+                                                    cellpadding="0" cellspacing="0" role="presentation"
+                                                    style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td class="pad"
+                                                                style="vertical-align: middle; color: #1e0e4b; font-family: 'Inter', sans-serif; font-size: 15px; padding-bottom: 5px; padding-top: 5px; text-align: center;">
+
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
