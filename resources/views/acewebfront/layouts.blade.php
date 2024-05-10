685886c2beb64a559e31988bf7e516d413d30514
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2024-05-10 15:50:47 +0800

    update

--- resources/views/acewebfront/layouts.blade.php
+++ resources/views/acewebfront/layouts.blade.php
@@ -55,7 +55,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/Flip.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link href="{{ static_asset('aceweb') }}/assets/ace/ipadace1.css" rel="stylesheet" />
-    <link href="{{ static_asset('aceweb') }}/assets/gosford/gosford-style.css" rel="stylesheet" />
+    <link href="{{ static_asset('aceweb') }}/assets/gosford/gosford.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
