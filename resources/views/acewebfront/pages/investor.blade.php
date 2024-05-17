75f14fc8a753485d3f8453331d015bfd2f06edff
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2024-05-18 00:03:48 +0800

    update

--- resources/views/acewebfront/pages/investor.blade.php
+++ resources/views/acewebfront/pages/investor.blade.php
@@ -3,16 +3,8 @@
     <meta property="og:image" content="{{ uploaded_asset(get_setting('system_logo_white')) }}">
 @endsection
 @section('content')
-
-        <iframe src="https://feytech.listedcompany.com/home.html" style="width: 100%;" id="myIframe"></iframe>
-        <script>
-            // Selecting the iframe element
-            var iframe = document.getElementById("myIframe");
-            
-            // Adjusting the iframe height onload event
-            iframe.onload = function(){
-                iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-            }
-            </script>
-    
+    <main>
+        <iframe class="iframe-investor" id="myIframe" src="https://feytech.listedcompany.com/home.html"
+            width="100%"></iframe>
+    </main>
 @endsection
